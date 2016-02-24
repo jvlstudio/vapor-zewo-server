ade4ec202df8c6fb6ff4cfb743392e177129f6b5
Tanner Nelson <tanner@bluebite.com>
2016-02-23 21:22:42 -0500

    add dependencies

--- Package.swift
+++ Package.swift
@@ -4,6 +4,11 @@ let package = Package(
     name: "VaporZewoServer",
     dependencies: [
         .Package(url: "https://github.com/Zewo/HTTPServer.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0)
+        .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0),
+
+        //temporary package dependencies due to SPM bug
+        .Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0),
+        .Package(url: "https://github.com/Zewo/CHTTPParser.git", majorVersion: 0),
+        .Package(url: "https://github.com/Zewo/CLibvenice.git", majorVersion: 0),
     ]
 )
